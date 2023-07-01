427d0577911a41ac8ddda3b2eb5ef54f5e1016ae
Tory <88531407+1two13@users.noreply.github.com>
2023-07-02 01:04:26 +0900

    design: 하단 탭바 배경색 추가

--- src/components/common/navBar/TabBar.jsx
+++ src/components/common/navBar/TabBar.jsx
@@ -139,7 +139,7 @@ const TabBar = () => {
   ];
 
   return (
-    <div className="fixed px-[15px] bottom-0 z-50 h-[85px] w-full border-t-[0.5px] border-solid border-[#A4A4A4]">
+    <div className="fixed bg-white px-[15px] bottom-0 z-50 h-[85px] w-full border-t-[0.5px] border-solid border-[#A4A4A4]">
       <ul className="flex justify-between">
         {Tabs.map((tab) => (
           <li
